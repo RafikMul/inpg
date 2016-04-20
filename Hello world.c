b51e3cac364e0e2465d5a1cbfcaabae3cc7d4d39
Pegas868 <kuba.pegza@gmail.com>
2016-04-20 08:57:34 +0200

    Added files via upload

---
+++ Hello world.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+int main()
+{
+	printf("Orki\n");
+	sleep(3);
+	system("cls");
+	printf("Orki z majorki\n");
+	sleep(3);
+	system("cls");
+	printf("Orki z poznania\n");
+	sleep(3);
+	system("cls");
+	printf("I ze stalowej woli\n");
+	sleep(3);
+	system("cls");
+	getchar();
+	return 0;
+}
