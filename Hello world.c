636183c80fe56c5d1df77a5306ecb85af2a36002
Baciek <maciekinpg@gmail.com>
2016-04-20 09:21:43 +0200

    klu gykljugbkjju

--- Hello world.c
+++ Hello world.c
@@ -15,6 +15,12 @@ int main()
 	printf("I ze stalowej woli\n");
 	sleep(3);
 	system("cls");
-	getchar();
+	getchar();bhjg;kblj
+bkjbvkjbklj
+kjbhkljblj'/bn
+hnjlbhljblj
+o;lhnjk
+'npolbhi
+b'ohu tvluivkj tfuoyl fluyf ylu drty yfkughcfluy ygljukljf li hfky yflu yflju gfkukigfvjyghf yi gi'  hi
 	return 0;
 }
